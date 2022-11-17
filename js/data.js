69c530c81f4813f486023118c0940dd32aef3159
tomasmperea <tomas.mperea@gmail.com>
2022-11-17 08:59:58 -0300

    modificaciones para la 3° entrega

--- js/data.js
+++ js/data.js
@@ -1,7 +1,8 @@
 // ARRAY DE OBJETOS (PRODUCTOS)
 
 class Remeras {
-    constructor(nombre, precio, foto) {
+    constructor(id, nombre, precio, foto) {
+      this.id = id;
       this.nombre = nombre;
       this.precio = precio;
       this.foto = foto;
@@ -15,15 +16,16 @@ class Remeras {
     }
   }
   
-  const remera1 = new Remeras("SUDADERA META ART GALLERY FABA 03 [ BLANCO ]", 5025, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/titan-faba_03-blanco-dorso1-69532e3aecb19db81916650837607662-640-0.jpg");
-  const remera2 = new Remeras("REMERA TITAN ART SUPPORT [ PIRANHA-3 BLANCO ]", 6500, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/551-ffbe9f11b54a809ab116566132918140-640-0.jpg");
-  const remera3 = new Remeras("REMERA SUNWAVES [ PROCESO J02 ]", 7610, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/remera-sunwaves-remera-batik-remera-procesada-remera-oversize-remeron-remera-manchada-remera-grande-91-1816729a6fb5eeb7a116643808165838-640-0.jpg");
-  const remera4 = new Remeras("REMERA META ART GALLERY CASTIGLIONI 02 [ PROCESO ]", 8950, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/remera-batik-estampada-aguila-dragon-21-81e2a5f9b602ca2c5916667100705047-640-0.jpg");
+  const remera1 = new Remeras("remera1","SUDADERA META ART GALLERY FABA 03 [ BLANCO ]", 5025, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/titan-faba_03-blanco-dorso1-69532e3aecb19db81916650837607662-640-0.jpg");
+  const remera2 = new Remeras("remera2","REMERA TITAN ART SUPPORT [ PIRANHA-3 BLANCO ]", 6500, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/551-ffbe9f11b54a809ab116566132918140-640-0.jpg");
+  const remera3 = new Remeras("remera3","REMERA SUNWAVES [ PROCESO J02 ]", 7610, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/remera-sunwaves-remera-batik-remera-procesada-remera-oversize-remeron-remera-manchada-remera-grande-91-1816729a6fb5eeb7a116643808165838-640-0.jpg");
+  const remera4 = new Remeras("remera4","REMERA META ART GALLERY CASTIGLIONI 02 [ PROCESO ]", 8950, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/remera-batik-estampada-aguila-dragon-21-81e2a5f9b602ca2c5916667100705047-640-0.jpg");
 
   
   
   class Medias {
-    constructor(nombre, precio, foto) {
+    constructor(id, nombre, precio, foto) {
+      this.id = id;
       this.nombre = nombre;
       this.precio = precio;
       this.foto = foto;
@@ -37,10 +39,10 @@ class Remeras {
     }
   }
   
-  const media1 = new Medias("MEDIA SUNDAY [ PRINT 50 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-negras-ojo2-21b6dc39a44e5ad83a16541968917487-640-0.jpg");
-  const media2 = new Medias("MEDIA SUNDAY [ PRINT I- 1 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-negras-rayas-blancas2-f4ca961fd3e72a1ab616594673246646-640-0.jpg");
-  const media3 = new Medias("MEDIA SUNDAY [ PRINT 58 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-negras-rosa-11-435370a69322c77cb616561738047654-640-0.jpg");
-  const media4 = new Medias("MEDIA SUNDAY [ PRINT 67 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-blancas-rayas-azul-roja2-0bd03ed791cc03119d16590187910756-640-0.jpg");
+  const media1 = new Medias("media1", "MEDIA SUNDAY [ PRINT 50 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-negras-ojo2-21b6dc39a44e5ad83a16541968917487-640-0.jpg");
+  const media2 = new Medias("media2", "MEDIA SUNDAY [ PRINT I- 1 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-negras-rayas-blancas2-f4ca961fd3e72a1ab616594673246646-640-0.jpg");
+  const media3 = new Medias("media3", "MEDIA SUNDAY [ PRINT 58 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-negras-rosa-11-435370a69322c77cb616561738047654-640-0.jpg");
+  const media4 = new Medias("media4", "MEDIA SUNDAY [ PRINT 67 ]", 890, "https://d3ugyf2ht6aenh.cloudfront.net/stores/188/770/products/medias-blancas-rayas-azul-roja2-0bd03ed791cc03119d16590187910756-640-0.jpg");
   
   
   const remeras = [
@@ -51,7 +53,9 @@ class Remeras {
     media1, media2, media3, media4
   ];
 
-  const carrito = [];
+  const productos = remeras.concat(medias)
+
+
 
 
   
--- js/main.js
+++ js/main.js
@@ -1,134 +1,201 @@
 
-// SELECTORES /VARIABLES
+// SELECTORES / VARIABLES
 
 const cardContainer1 = document.querySelector("#cardContainer1")
 const cardContainer2 = document.querySelector("#cardContainer2")
 const botonLogin = document.querySelector("#botonLogin")
 const consultarCarrito = document.querySelector("#consultarCarrito")
 
+let carrito;
+// let arrayUsuarios;
+
+if (localStorage.getItem("carrito")) {
+  carrito = JSON.parse(localStorage.getItem("carrito"))
+} else {
+  carrito = []
+}
+
+// if (localStorage.getItem("arrayUsuarios")) {
+//   arrayUsuarios = JSON.parse(localStorage.getItem("arrayUsuarios"))
+// } else {
+//   arrayUsuarios = []
+// }
+
+// console.log(arrayUsuarios);
+
+console.log("El carrito contiene: ", carrito);
+
 // FUNCIONES
 
-remeras.forEach(remera => {
-  const nuevaCard1= document.createElement("div");
-  nuevaCard1.className = "card";
-  nuevaCard1.innerHTML = `
-  
-      <img src= ${remera.foto}>
-      <div class="card-body">
-      <h5> ${remera.nombre} </h5>
-      <span> Precio: $${remera.precio} </span>
-      <br>
-      <br>
-      <button id="aniadirCarrito" class= p-1> Añadir al carrito </button
-      <a href="#">
-      </a>
-  
-  `
-  cardContainer1.append(nuevaCard1);
-});
+const renderizarProductos = () => {
+  remeras.forEach(remera => {
+    const nuevaCard1 = document.createElement("div");
+    const contenedorCards = document.createElement("div");
+    const cardBody = document.createElement("div")
+    const nombreRemera = document.createElement("h6")
+    const precio = document.createElement("h4")
+    const aniadirCarrito = document.createElement("button")
 
+    contenedorCards.classList.add("p-4")
+    nombreRemera.classList.add("p-2")
+    nuevaCard1.classList.add("card")
+    cardBody.classList.add("card-body")
+    aniadirCarrito.classList.add("aniadirCarrito")
+    aniadirCarrito.classList.add("p-2")
+    aniadirCarrito.setAttribute("data-id", remera.id)
+    aniadirCarrito.textContent = "Añadir al carrito"
+    nombreRemera.textContent = `${remera.nombre}`
+    precio.textContent = `$${remera.precio}`
 
-medias.forEach(media => {
-  const nuevaCard2= document.createElement("div");
-  nuevaCard2.className = "card";
-  nuevaCard2.innerHTML = `
+    nuevaCard1.innerHTML = `
+    
+        <img src= ${remera.foto} alt= ${remera.nombre}>
+        <a href="#">
+        </a>
+    
+    `
+    cardContainer1.append(contenedorCards);
+    contenedorCards.append(nuevaCard1);    
+    nuevaCard1.append(nombreRemera);
+    nuevaCard1.append(precio);
+    nuevaCard1.append(cardBody);
+    cardBody.append(aniadirCarrito);
+  });
   
-      <img src= ${media.foto}>
-      <div class="card-body">
-      <h5> ${media.nombre} </h5>
-      <span> Precio: $${media.precio} </span>
-      <br>
-      <br>
-      <button id="aniadirCarrito" class= p-1> Añadir al carrito </button
-      <a href="#">
-      </a>
-  
-  `
-  cardContainer2.append(nuevaCard2);
 
-});
+  medias.forEach(media => {
+    const nuevaCard2 = document.createElement("div");
+    const contenedorCards = document.createElement("div");
+    const cardBody = document.createElement("div")
+    const nombreMedia = document.createElement("h6")
+    const precio = document.createElement("h4")
+    const aniadirCarrito = document.createElement("button")
+
+    contenedorCards.classList.add("p-4")
+    nombreMedia.classList.add("p-2")
+    nuevaCard2.classList.add("card")
+    cardBody.classList.add("card-body")
+    aniadirCarrito.classList.add("aniadirCarrito")
+    aniadirCarrito.classList.add("p-2")
+    aniadirCarrito.setAttribute("data-id", media.id)
+    aniadirCarrito.textContent = "Añadir al carrito"
+    nombreMedia.textContent = `${media.nombre}`
+    precio.textContent = `$${media.precio}`
+
+    nuevaCard2.innerHTML = `
+    
+        <img src= ${media.foto} alt= ${media.nombre}>
+        <a href="#">
+        </a>
+    
+    `
+    cardContainer2.append(contenedorCards);
+    contenedorCards.append(nuevaCard2);
+    nuevaCard2.append(nombreMedia);
+    nuevaCard2.append(precio);
+    nuevaCard2.append(cardBody);
+    cardBody.append(aniadirCarrito);
+  });
+  
+  const buttonCarrito = document.querySelectorAll(".aniadirCarrito")
+  buttonCarrito.forEach((button) => {
+    button.addEventListener("click", agregarCarrito)
+  })
+}
+
+const agregarCarrito = (e) => {
+  const idProducto = e.target.getAttribute("data-id");
+  const idElegido = productos.find((producto) => producto.id === idProducto)
+
+  carrito.push(idElegido)
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+  console.log("El carrito contiene: ", carrito);
+
+}
+
+// puedo mejorar la seguridad del login ESTA ES LA VERSION QUE FUNCIONA
+
+const loginUser = () => { 
+  const nombreUsuarioStorage = localStorage.getItem("user")
+
+  if (nombreUsuarioStorage){
+    botonLogin.innerText = "Cerrar sesión"
+    alert(`Bienvenido humano ${nombreUsuarioStorage}`)
+  } else {
+    
+    let user = prompt("Ingrese su usuario: ");
+    let pass = prompt("Ingrese su contraseña: ");
+
+    while (user === "" || pass === "") {
+      alert("Usuario y/o contraseña incorrecta: ingrese nuevamente sus datos ");
+      if (user === "") {
+        user = prompt("Ingrese su usuario: ");
+      } else {
+        pass = prompt("Ingrese su contraseña: ");
+      }
+    }
+
+    localStorage.setItem("user", user)
+    botonLogin.innerText = "Cerrar sesión"
+    alert(`Bienvenido humano ${user}`)
+  }
+}
+
+// const agregarUsuarios = (e) => {
+//   nombreUsuario = e.target.getAttribute("nombre");
+//   nombreElegido = arrayUsuarios.find((usuario) => usuario.nombre === nombreUsuario);
+
+//   arrayUsuarios.push(nombreElegido);
+//   localStorage.setItem("arrayUsuarios", JSON.stringify(arrayUsuarios));
+//   console.log("arrayUsuarios");
+// }
+
+// const loginUser = () => { 
+//   const nombreUsuario = localStorage.getItem("usuario")
+
+//   if (nombreUsuario){
+//     botonLogin.innerText = "Cerrar sesión"
+//     alert(`Bienvenido humano ${nombreUsuario}`)
+//   } else {
+    
+//     let usuario = prompt("Ingrese su usuario: ");
+//     let password = prompt("Ingrese su contraseña: ");
+
+//     while (usuario === "" || password === "") {
+//       alert("Usuario y/o contraseña incorrecta: ingrese nuevamente sus datos ");
+//       if (usuario === "") {
+//         usuario = prompt("Ingrese su usuario: ");
+//       } else {
+//         password = prompt("Ingrese su contraseña: ");
+//       }
+//     }
+
+//     localStorage.setItem("usuario", usuario)
+//     botonLogin.innerText = "Cerrar sesión"
+//     alert(`Bienvenido humano ${usuario}`)
+//   }
+
+// }
+
+// const logoutUser = () => {
+//   localStorage.removeItem("usuario")
+//   alert(`Hasta la próxima ${usuario}! Esperamos verte de nuevo por aquí :)`)
+// }
 
 // LISTENERS
 
-botonLogin.addEventListener("click", () => { 
-  let user = prompt("Ingrese su usuario: ");
-  let pass = prompt("Ingrese su contraseña: ");
-
-  while (user === "" || pass === "") {
-    alert("Usuario y/o contraseña incorrecta: ingrese nuevamente sus datos ");
-    if (user === "") {
-      user = prompt("Ingrese su usuario: ");
-    } else {
-      pass = prompt("Ingrese su contraseña: ");
-    }
-  }
-  botonLogin.innerText = "Bienvenido " + user;
-
-  let consultaUsuario = prompt("Seleccione remeras o medias para ver el stock disponible: ").toLowerCase();
-
-if (consultaUsuario === "remeras" || consultaUsuario === "Remeras" || consultaUsuario === "REMERAS") {
-  remeras.forEach(remera => {
-    alert(remera.nombre + " " + " Precio sin IVA: " + "$"+ remera.precio)});
-  } else if (consultaUsuario === "medias" || consultaUsuario === "medias" || consultaUsuario === "MEDIAS") {
-    medias.forEach(media => {
-    alert(media.nombre + " " + " Precio sin IVA: " + "$"+ media.precio)})
-  } else {
-    alert("No comercializamos los productos ingresados.");
-  
-  }
-})
+botonLogin.addEventListener("click", loginUser)
 
 consultarCarrito.addEventListener("click", () => {
-  const totalCarrito = carrito.reduce((acumulador, carrito) => acumulador + carrito, 0);
+  const totalCarrito = carrito.reduce((acumulador, carrito) => acumulador + carrito.precio, 0);
 
-alert("El valor total del carrito es: " + "$" + (totalCarrito*1.21) + " IVA Final")
+alert("El valor total del carrito es: " + "$" + parseFloat(totalCarrito*1.21) + " IVA Final")
 
 })
+
+// botonLogin.addEventListener("click", logoutUser)
+
 
 // EJECUTORES
 
-
-// function aniadirCarrito() {
-//   alert("Usted ha seleccionado un ítem. Total carrito: " + "$" + carrito);
-//   medioPago = prompt(
-//     "Seleccione su medio de pago: efectivo o tarjeta"
-//   ).toLowerCase();
-//   while (medioPago == "") {
-//     alert("No ha ingresado una opción. Vuelva a intentarlo");
-//     medioPago = prompt(
-//       "Seleccione su medio de pago: efectivo o tarjeta"
-//     ).toLowerCase();
-//   }
-//   if (medioPago === !"efectivo" && medioPago === !"tarjeta") {
-//     alert("No ha ingresado una opción. Vuelva a intentarlo");
-//     medioPago = prompt(
-//       "Seleccione su medio de pago: efectivo o tarjeta"
-//     ).toLowerCase();
-//   } else if (medioPago === "tarjeta") {
-//     tarjeta = prompt(
-//       "Seleccione su tarjeta: santander, amex, nacion, otras: "
-//     ).toLowerCase();
-//     switch (tarjeta) {
-//       case "santander":
-//         alert("Valor total con 75% de descuento: " + "$" + carrito * 0.25);
-//         break;
-
-//       case "amex":
-//         alert("Valor total con 50% de descuento: " + "$" + carrito * 0.5);
-//         break;
-
-//       case "nacion":
-//         alert("Valor total con 25% de descuento: " + "$" + carrito * 0.75);
-//         break;
-
-//       default:
-//         alert(
-//           "La tarjeta que ingresó no posee descuentos: su pago total es de: " +
-//             "$" +
-//             carrito
-//         );
-//     }
-//   } else {
-//     alert("Su pago en efectivo es de: " + "$" + carrito);
-//   }
-// }
+renderizarProductos();
